d6404f19b695a1127c5ddb8abeb630a2a9d951d9
Marifer <mfvl.1186@gmail.com>
2023-01-13 21:33:07 -0300

    enlistado de TODO en ABP M2

--- Trabajos/M2/ValidadorUsuario/src/App.java
+++ Trabajos/M2/ValidadorUsuario/src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
+
+
+        
         System.out.println("Hello, World!");
     }
 }
+// TODO  Rescatar datos básicos de clientes
+// TODO  Get data from input 
+// TODO Create variables
+// TODO  Get RUT ==> Number < 99.999.999, Int
+// TODO  Get Nombres==>  requiered, String
+// TODO  Get Apellidos==> requiered, String
+// TODO  Get Teléfono: <= 15 char, String
+// TODO  Get AFP ==> requiered, String
+// TODO  Get Sistema de salud ==>  1 (Fonasa) o 2 (Isapre), Menu with int
+// TODO  Get  Dirección: <=50 char, String
+// TODO  Get  Comuna: requiered, String
+// TODO  Get  Edad: <120 años
+
+
+// TODO   Validate data
+// TODO  Print error message if neccesary
+// TODO Print all data in console. 
+
