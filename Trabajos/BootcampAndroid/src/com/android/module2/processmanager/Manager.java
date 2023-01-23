e66f9ff55ee01fd0afb2b48769e106d016214cc1
Marifer <mfvl.1186@gmail.com>
2023-01-22 23:06:50 -0300

    TO DO stablished

---
+++ Trabajos/BootcampAndroid/src/com/android/module2/processmanager/Manager.java
@@ -0,0 +1,27 @@
+package com.android.module2.processmanager;
+
+public class Manager {
+    public static void main(String[] args) {
+        //Empresa: Asesorías en prevención de riesgos ==> Necesita: Sistema de información
+        //Todos los datos son obligatorios.
+        // TODO: Info de cliente(empresa) p/capacitación: 
+        // TODO: >>registro del RUT,
+        // TODO: >>registro del nombre, 
+        // TODO: >>registro de dirección,
+        // TODO: >>registro de comuna 
+        // TODO: >>registro de número de teléfono. 
+        // TODO: Info de Capacitación:
+        // TODO: >>registro de día, 
+        // TODO: >>registro de hora, 
+        // TODO: >>registro de lugar, 
+        // TODO: >>registro de duración y 
+        // TODO: >>registro de cantidad de asistentes:
+        // TODO: >>>> Validar que cantidad de asistentes > 0
+        // TODO: >>>> solicitar nombre y 
+        // TODO: >>>> solicitar edad 
+        // TODO: Mostrar por pantalla datos de empresa , 
+        // TODO: Mostrar por pantalla datos de capacitación, y  
+        // TODO: Mostrar por pantalla Cantidad de personas < 25 años, entre 26 y 35 años y > 35 años.
+    }
+    
+}
